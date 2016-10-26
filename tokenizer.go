68855b05418c5fa029df35452cdd674aff110b6d
Michael Clifford <cliffom@gmail.com>
2016-10-26 16:13:30 -0700

    :penguin: Happy Path for basic tokenization

--- tokenizer.go
+++ tokenizer.go
@@ -1,10 +1,71 @@
 package tokenex
 
-func Tokenize(data string, tokenScheme int) string {
+import "encoding/json"
+
+type TokenResponse struct {
+  Error string
+  ReferenceNumber string
+  Success bool
+  Token string
+}
+
+type ValueResponse struct {
+  Error string
+  ReferenceNumber string
+  Success bool
+  Value string
+}
+
+type ValidateResponse struct {
+  Error string
+  ReferenceNumber string
+  Success bool
+  Valid bool
+}
+
+type DeleteResponse struct {
+  Error string
+  ReferenceNumber string
+  Success bool
+}
+
+func Tokenize(data string, tokenScheme int) TokenResponse {
   tData := map[string]interface{}{
     "Data": data,
     "TokenScheme": tokenScheme,
   }
   data = request("Tokenize", tData)
-  return data
+  response := TokenResponse{}
+  json.Unmarshal([]byte(data), &response)
+  return response
+}
+
+func Detokenize(token string) ValueResponse {
+  tData := map[string]interface{}{
+    "Token": token,
+  }
+  data := request("Detokenize", tData)
+  response := ValueResponse{}
+  json.Unmarshal([]byte(data), &response)
+  return response
+}
+
+func Validate(token string) ValidateResponse {
+  tData := map[string]interface{}{
+    "Token": token,
+  }
+  data := request("ValidateToken", tData)
+  response := ValidateResponse{}
+  json.Unmarshal([]byte(data), &response)
+  return response
+}
+
+func Delete(token string) DeleteResponse {
+  tData := map[string]interface{}{
+    "Token": token,
+  }
+  data := request("DeleteToken", tData)
+  response := DeleteResponse{}
+  json.Unmarshal([]byte(data), &response)
+  return response
 }
