0f68d05090a71b8a79607897416bed1adebab0de
Michael Clifford <cliffom@gmail.com>
2016-10-26 16:18:12 -0700

    :penguin: gofmt

--- tokenizer.go
+++ tokenizer.go
@@ -3,69 +3,69 @@ package tokenex
 import "encoding/json"
 
 type TokenResponse struct {
-  Error string
-  ReferenceNumber string
-  Success bool
-  Token string
+	Error           string
+	ReferenceNumber string
+	Success         bool
+	Token           string
 }
 
 type ValueResponse struct {
-  Error string
-  ReferenceNumber string
-  Success bool
-  Value string
+	Error           string
+	ReferenceNumber string
+	Success         bool
+	Value           string
 }
 
 type ValidateResponse struct {
-  Error string
-  ReferenceNumber string
-  Success bool
-  Valid bool
+	Error           string
+	ReferenceNumber string
+	Success         bool
+	Valid           bool
 }
 
 type DeleteResponse struct {
-  Error string
-  ReferenceNumber string
-  Success bool
+	Error           string
+	ReferenceNumber string
+	Success         bool
 }
 
 func Tokenize(data string, tokenScheme int) TokenResponse {
-  tData := map[string]interface{}{
-    "Data": data,
-    "TokenScheme": tokenScheme,
-  }
-  data = request("Tokenize", tData)
-  response := TokenResponse{}
-  json.Unmarshal([]byte(data), &response)
-  return response
+	tData := map[string]interface{}{
+		"Data":        data,
+		"TokenScheme": tokenScheme,
+	}
+	data = request("Tokenize", tData)
+	response := TokenResponse{}
+	json.Unmarshal([]byte(data), &response)
+	return response
 }
 
 func Detokenize(token string) ValueResponse {
-  tData := map[string]interface{}{
-    "Token": token,
-  }
-  data := request("Detokenize", tData)
-  response := ValueResponse{}
-  json.Unmarshal([]byte(data), &response)
-  return response
+	tData := map[string]interface{}{
+		"Token": token,
+	}
+	data := request("Detokenize", tData)
+	response := ValueResponse{}
+	json.Unmarshal([]byte(data), &response)
+	return response
 }
 
 func Validate(token string) ValidateResponse {
-  tData := map[string]interface{}{
-    "Token": token,
-  }
-  data := request("ValidateToken", tData)
-  response := ValidateResponse{}
-  json.Unmarshal([]byte(data), &response)
-  return response
+	tData := map[string]interface{}{
+		"Token": token,
+	}
+	data := request("ValidateToken", tData)
+	response := ValidateResponse{}
+	json.Unmarshal([]byte(data), &response)
+	return response
 }
 
 func Delete(token string) DeleteResponse {
-  tData := map[string]interface{}{
-    "Token": token,
-  }
-  data := request("DeleteToken", tData)
-  response := DeleteResponse{}
-  json.Unmarshal([]byte(data), &response)
-  return response
+	tData := map[string]interface{}{
+		"Token": token,
+	}
+	data := request("DeleteToken", tData)
+	response := DeleteResponse{}
+	json.Unmarshal([]byte(data), &response)
+	return response
 }
