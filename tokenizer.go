34ec6969f4b621e886ab0310ee8f8087ed30501b
Michael Clifford <cliffom@gmail.com>
2016-10-27 09:33:23 -0700

    :penguin: more cleanups

--- tokenizer.go
+++ tokenizer.go
@@ -3,69 +3,69 @@ package tokenex
 import "encoding/json"
 
 type (
-	TokenExResponse struct {
+	tokenexResponse struct {
 		Error           string
 		ReferenceNumber string
 		Success         bool
 	}
 
-	TokenResponse struct {
-		TokenExResponse
+	tokenResponse struct {
+		tokenexResponse
 		Token string
 	}
 
-	ValueResponse struct {
-		TokenExResponse
+	valueResponse struct {
+		tokenexResponse
 		Value string
 	}
 
-	ValidateResponse struct {
-		TokenExResponse
+	validateResponse struct {
+		tokenexResponse
 		Valid bool
 	}
 
-	DeleteResponse struct {
-		TokenExResponse
+	deleteResponse struct {
+		tokenexResponse
 	}
 )
 
-func Tokenize(data string, tokenScheme int) TokenResponse {
+func Tokenize(data string, tokenScheme int) tokenResponse {
 	tData := map[string]interface{}{
 		"Data":        data,
 		"TokenScheme": tokenScheme,
 	}
 	data = request("Tokenize", tData)
-	response := TokenResponse{}
+	response := tokenResponse{}
 	json.Unmarshal([]byte(data), &response)
 	return response
 }
 
-func Detokenize(token string) ValueResponse {
+func Detokenize(token string) valueResponse {
 	tData := map[string]interface{}{
 		"Token": token,
 	}
 	data := request("Detokenize", tData)
-	response := ValueResponse{}
+	response := valueResponse{}
 	json.Unmarshal([]byte(data), &response)
 	return response
 }
 
-func Validate(token string) ValidateResponse {
+func Validate(token string) validateResponse {
 	tData := map[string]interface{}{
 		"Token": token,
 	}
 	data := request("ValidateToken", tData)
-	response := ValidateResponse{}
+	response := validateResponse{}
 	json.Unmarshal([]byte(data), &response)
 	return response
 }
 
-func Delete(token string) DeleteResponse {
+func Delete(token string) deleteResponse {
 	tData := map[string]interface{}{
 		"Token": token,
 	}
 	data := request("DeleteToken", tData)
-	response := DeleteResponse{}
+	response := deleteResponse{}
 	json.Unmarshal([]byte(data), &response)
 	return response
 }
