369b690cc5a0f7b8e767121b58a5b2d0e952549d
Michael Clifford <cliffom@gmail.com>
2016-10-28 21:54:46 -0700

    :penguin: clean and consolidate

--- response.go
+++ response.go
@@ -2,12 +2,14 @@ package tokenex
 
 import (
 	"encoding/json"
+	"errors"
 	"github.com/parnurzeal/gorequest"
 	"log"
 )
 
 type (
 	BaseResponse struct {
+		Data            map[string]interface{}
 		Error           string
 		ReferenceNumber string
 		Success         bool
@@ -33,7 +35,29 @@ type (
 	}
 )
 
-func (b *BaseResponse) get(data map[string]interface{}) string {
+func (b *BaseResponse) result(v interface{}) error {
+	err := json.Unmarshal([]byte(request(b.Data)), &v)
+	errStr := ""
+	if err == nil {
+		switch v.(type) {
+		case *TokenResponse:
+			errStr = v.(*TokenResponse).Error
+		case *ValueResponse:
+			errStr = v.(*ValueResponse).Error
+		case *ValidateResponse:
+			errStr = v.(*ValidateResponse).Error
+		case *DeleteResponse:
+			errStr = v.(*DeleteResponse).Error
+		}
+		if errStr != "" {
+			err = errors.New(errStr)
+		}
+	}
+
+	return err
+}
+
+func request(data map[string]interface{}) string {
 	if config.baseUrl == "" {
 		log.Fatalf("config.baseUrl not set")
 	} else if config.id == "" {
--- tokenizer.go
+++ tokenizer.go
@@ -1,93 +1,53 @@
 package tokenex
 
-import (
-	"encoding/json"
-	"errors"
-)
-
 func Tokenize(data string, tokenScheme int) (TokenResponse, error) {
-	var err error
-	tData := map[string]interface{}{
+	response := TokenResponse{}
+	response.Data = map[string]interface{}{
 		"Action":      "Tokenize",
 		"Data":        data,
 		"TokenScheme": tokenScheme,
 	}
-	response := TokenResponse{}
-	data = response.get(tData)
-
-	json.Unmarshal([]byte(data), &response)
-	if response.Error != "" {
-		err = errors.New(response.Error)
-	}
-
+	err := response.result(&response)
 	return response, err
 }
 
 func TokenizeFromEncryptedValue(data string, tokenScheme int) (TokenResponse, error) {
-	var err error
-	tData := map[string]interface{}{
+	response := TokenResponse{}
+	response.Data = map[string]interface{}{
 		"Action":       "TokenizeFromEncryptedValue",
 		"EcryptedData": data,
 		"TokenScheme":  tokenScheme,
 	}
-	response := TokenResponse{}
-	data = response.get(tData)
-
-	json.Unmarshal([]byte(data), &response)
-	if response.Error != "" {
-		err = errors.New(response.Error)
-	}
-
+	err := response.result(&response)
 	return response, err
 }
 
 func Detokenize(token string) (ValueResponse, error) {
-	var err error
-	tData := map[string]interface{}{
+	response := ValueResponse{}
+	response.Data = map[string]interface{}{
 		"Action": "Detokenize",
 		"Token":  token,
 	}
-	response := ValueResponse{}
-	data := response.get(tData)
-
-	json.Unmarshal([]byte(data), &response)
-	if response.Error != "" {
-		err = errors.New(response.Error)
-	}
-
+	err := response.result(&response)
 	return response, err
 }
 
 func Validate(token string) (ValidateResponse, error) {
-	var err error
-	tData := map[string]interface{}{
+	response := ValidateResponse{}
+	response.Data = map[string]interface{}{
 		"Action": "ValidateToken",
 		"Token":  token,
 	}
-	response := ValidateResponse{}
-	data := response.get(tData)
-
-	json.Unmarshal([]byte(data), &response)
-	if response.Error != "" {
-		err = errors.New(response.Error)
-	}
-
+	err := response.result(&response)
 	return response, err
 }
 
 func Delete(token string) (DeleteResponse, error) {
-	var err error
-	tData := map[string]interface{}{
+	response := DeleteResponse{}
+	response.Data = map[string]interface{}{
 		"Action": "DeleteToken",
 		"Token":  token,
 	}
-	response := DeleteResponse{}
-	data := response.get(tData)
-
-	json.Unmarshal([]byte(data), &response)
-	if response.Error != "" {
-		err = errors.New(response.Error)
-	}
-
+	err := response.result(&response)
 	return response, err
 }
